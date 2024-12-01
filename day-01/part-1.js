50679689793097f09ca2789a47609baf6d9903f6
Marco Pollacci <pollacci@gmail.com>
2024-12-01 11:28:54 +0100

    feat(advent): :white_check_mark: day one

--- day-01/part-1.js
+++ day-01/part-1.js
@@ -1,2 +1,14 @@
 import input from "../common.js";
-console.log("🚀 ~ input:", input);
+
+const initialArray = input.split("\n");
+
+const firstColumn = initialArray.map((row) => row.split(/\s+/)[0]).sort();
+const secondColumn = initialArray.map((row) => row.split(/\s+/)[1]).sort();
+
+// difference betwwen first and second column
+const result = firstColumn.reduce((acc, curr, index) => {
+  // lazy is the best
+  return acc + Math.abs(curr - secondColumn[index]);
+}, 0);
+
+console.log("🚀 ~ input:", result);
--- day-01/part-2.js
+++ day-01/part-2.js
@@ -1,2 +1,15 @@
 import input from "../common.js";
-console.log("🚀 ~ input:", input);
+
+const initialArray = input.split("\n");
+
+const firstColumn = initialArray.map((row) => row.split(/\s+/)[0]).sort();
+const secondColumn = initialArray.map((row) => row.split(/\s+/)[1]).sort();
+
+const total = firstColumn.reduce((acc, curr) => {
+  //search how many time a number appears in second column
+  const times = secondColumn.filter(
+    (secondNumber) => secondNumber === curr
+  ).length;
+  return acc + times * curr;
+}, 0);
+console.log("🚀 ~ total ~ total:", total);
